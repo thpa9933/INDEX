f296d656d5f137c26dd2e5bc80ab99c5d07a5ba8
Thomas Payne <thpa9933@colorado.edu>
2018-05-03 16:21:17 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//
+//
+// $('.P1').on('click',function(){
+//   console.log("it works");
+//   $('.P1').toggleClass('active');
+// })
+//
+// $('.P2').on('click',function(){
+//   console.log("it works");
+//   $('.P2').toggleClass('active');
+// })
+//
+// $('.P3').on('click',function(){
+//   console.log("it works");
+//   $('.P3').toggleClass('active');
+// })
+//
+// $('.P4').on('click',function(){
+//   console.log("it works");
+//   $('.P4').toggleClass('active');
+// })
